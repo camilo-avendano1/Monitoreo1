a6765c6a9f6303a2629ff2bc5a93b903cacfb705
camilo av <camilo.avendano1@udea.edu.co>
2023-10-10 12:50:00 -0500

    creacion del proyecto

---
+++ src/main/java/com/udea/fe/monitoreo1/Monitoreo1Application.java
@@ -0,0 +1,13 @@
+package com.udea.fe.monitoreo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Monitoreo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Monitoreo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/udea/fe/monitoreo1/Monitoreo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.fe.monitoreo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Monitoreo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
