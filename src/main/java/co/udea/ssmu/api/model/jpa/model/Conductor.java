230daa5bb690b0134521f10e7fc387214b7d2690
camilo av <camilo.avendano1@udea.edu.co>
2023-10-25 19:53:22 -0500

    prueba controller

--- src/main/java/co/udea/ssmu/api/model/jpa/model/Conductor.java
+++ src/main/java/co/udea/ssmu/api/model/jpa/model/Conductor.java
@@ -6,7 +6,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
